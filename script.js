b63fafa55696bc27d5ef75abb41405bfc08230a8
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-11-04 10:25:38 -0400

    changed stuff around

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
 
 // added variables for the things that will consist of a password
 // added an array called possibleCharacters
-var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
-var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?'];
-var possibleCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?']
+// var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+// var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?'];
+var possible = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?']
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // added a function
-function generatePassword() {
-  // all code goes here
-  // added my first prompt
-  // added the rest of my prompts
-  var promptOne = prompt('Please choose a password between 8 and 128 characters.');
+function generatePassword() { 
+// all code goes here
+// added my first prompt
+// added the rest of my prompts
+
+
+var promptOne= prompt('Please choose a password between 8 and 128 characters.')
 // added an if else statment 
 // moved my alerts around
-  if (promptOne >8 && promptOne <129) {
-    console.log('true');
- } 
- else {(promptOne<8 || promptOne >129) 
-    alert("Password must be between 8 and 128 characters")
-  }
-  var promptTwo = prompt('Would you like to add special characters?')
-// added an alert message
-  if (promptTwo >=1) {
-    console.log(true);
-  }
-    
-  else { (promptTwo <1) 
-    alert("Password must contain at least one special character." )
-  }
-  var promptThree = prompt('Would you like to add uppercase letters?')
-  var promptFour = prompt('Would you like to include lowecase letters?')
-
- 
-
+if (promptOne > 8 && promptOne < 129) {
   
+}
+
+else {
+  (promptOne < 8 || promptOne > 129)
+  alert("Password must be between 8 and 128 characters")
+}
+var promptTwo = prompt('Would you like to add special characters?')
+// // added an alert message
+
+var promptThree = prompt('Would you like to add uppercase letters?')
+var promptFour = prompt('Would you like to include lowercase letters?')
+
+// if (promptTwo >= 1) {
+//   console.log(true);
+// }
+// else {
+//   (promptTwo < 1)
+//   alert("Password must contain at least one special character.")
+// }
+
+var atRandom = Math.floor(Math.random() * possible.length);
+var display = possible[atRandom];
+
+
+
+
+
+
+
+
+
 
 
 
@@ -52,8 +66,8 @@ function generatePassword() {
 
 // Returned something
 return "Random Password Will Appear Here";
-}
 
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -65,4 +79,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
