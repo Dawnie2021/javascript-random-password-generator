e15d7b9819e581b512aa94c0eda0664ed1a364d9
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-11-04 04:36:14 -0400

    added an if else statement

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 // added variables for the things that will consist of a password
-var numbersArray= ['0','1','2','3','4','5','6','7','8','9'];
-var lowercaseLetters= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var uppercaseLetters= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
-var specialCharacters= ['!','@','#','$','%','^','&','*','(',')','?'];
+var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?'];
 
 
 // Assignment Code
@@ -11,13 +11,28 @@ var generateBtn = document.querySelector("#generate");
 
 // added a function
 function generatePassword() {
-// all code goes here
-// added my first prompt
-// added the rest of my prompts
-var randomUser= prompt ('Please choose a password between 8 and 128 characters.');
-var randomUser= prompt ('Would you like to add special characters?')
-var randomUser= prompt ('Would you like to add uppercase letters?')
-var randomUser= prompt ('Would you like to include lowecase letters?')
+  // all code goes here
+  // added my first prompt
+  // added the rest of my prompts
+  var promptOne = prompt('Please choose a password between 8 and 128 characters.');
+  var promptTwo = prompt('Would you like to add special characters?')
+  var promptThree = prompt('Would you like to add uppercase letters?')
+  var promptFour = prompt('Would you like to include lowecase letters?')
+// added an if else statment 
+  if (promptOne >8 && promptOne <129) {
+    console.log('true');
+ } 
+ if (promptOne<8 || promptOne >129) {
+    alert("Password must be between 8 and 128 characters")
+  }
+    
+  
+
+
+
+
+
+
 
 
 
@@ -28,6 +43,7 @@ var randomUser= prompt ('Would you like to include lowecase letters?')
 // Returned something
 return "Random Password Will Appear Here";
 }
+
 
 // Write password to the #password input
 function writePassword() {
